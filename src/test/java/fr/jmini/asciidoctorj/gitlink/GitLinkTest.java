49bac194cc9cd2808b50ce9af2888854db47e458
Jérémie Bresson <jeremie.bresson@unblu.com>
2022-09-07 22:46:52 +0200

    SonarQube fixes (#15)
    * Remove unnecessary public keyword
    * Use @ParameterizedTest

--- src/test/java/fr/jmini/asciidoctorj/gitlink/GitLinkTest.java
+++ src/test/java/fr/jmini/asciidoctorj/gitlink/GitLinkTest.java
@@ -25,17 +25,20 @@ import org.asciidoctor.Asciidoctor.Factory;
 import org.asciidoctor.log.LogRecord;
 import org.asciidoctor.log.Severity;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class GitLinkTest {
+class GitLinkTest {
 
-  @Test
-  public void testSimple() throws Exception {
-    List<LogRecord> logs = runTest("test_simple");
+  @ParameterizedTest
+  @ValueSource(strings = {"test_simple", "test_empty3", "test_with_all-1", "test_with_all-2", "test_with_branch-2", "test_with_link_text", "test_with_branch-1"})
+  void testNoExpectedLogs(String name) throws Exception {
+    List<LogRecord> logs = runTest(name);
     assertThat(logs).isEmpty();
   }
 
   @Test
-  public void testEmpty() throws Exception {
+  void testEmpty() throws Exception {
     List<LogRecord> logs = runTest("test_empty");
     assertThat(logs).hasSize(1);
     LogRecord log = logs.get(0);
@@ -44,7 +47,7 @@ public class GitLinkTest {
   }
 
   @Test
-  public void testEmpty2() throws Exception {
+  void testEmpty2() throws Exception {
     List<LogRecord> logs = runTest("test_empty2");
     assertThat(logs).hasSize(1);
     LogRecord log = logs.get(0);
@@ -53,13 +56,7 @@ public class GitLinkTest {
   }
 
   @Test
-  public void testEmpty3() throws Exception {
-    List<LogRecord> logs = runTest("test_empty3");
-    assertThat(logs).isEmpty();
-  }
-
-  @Test
-  public void testRepositoryMissing() throws Exception {
+  void testRepositoryMissing() throws Exception {
     List<LogRecord> logs = runTest("test_repository_missing");
     assertThat(logs).hasSize(2);
     LogRecord log1 = logs.get(0);
@@ -68,36 +65,6 @@ public class GitLinkTest {
     LogRecord log2 = logs.get(1);
     assertThat(log2.getSeverity()).isEqualTo(Severity.DEBUG);
     assertThat(log2.getMessage()).isEqualTo("git-link: The mode is not defined, using 'view' as fallback.");
-  }
-
-  @Test
-  public void testWithBranch1() throws Exception {
-    List<LogRecord> logs = runTest("test_with_branch-1");
-    assertThat(logs).isEmpty();
-  }
-
-  @Test
-  public void testWithBranch2() throws Exception {
-    List<LogRecord> logs = runTest("test_with_branch-2");
-    assertThat(logs).isEmpty();
-  }
-
-  @Test
-  public void testWithLinkText() throws Exception {
-    List<LogRecord> logs = runTest("test_with_link_text");
-    assertThat(logs).isEmpty();
-  }
-
-  @Test
-  public void testWithAll1() throws Exception {
-    List<LogRecord> logs = runTest("test_with_all-1");
-    assertThat(logs).isEmpty();
-  }
-
-  @Test
-  public void testWithAll2() throws Exception {
-    List<LogRecord> logs = runTest("test_with_all-2");
-    assertThat(logs).isEmpty();
   }
 
   private List<LogRecord> runTest(String fileName) throws IOException, URISyntaxException {
